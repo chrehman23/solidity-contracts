4612ab473e8e037ee09bf8cbd51dd7b551f43a34
Abdur Rehman <mrahman9pk@gmail.com>
2023-07-23 18:17:18 +0400

    fromating error

--- calling-other-contracts.sol
+++ calling-other-contracts.sol
@@ -4,26 +4,18 @@
 pragma solidity ^0.8.0;
 
 contract CallerContract {
-    function setX(
-        address _contractAdress,
-        string memory _x
-    ) public returns (bool) {
+    function setX(address _contractAdress, string memory _x) public returns (bool) {
         ContractA(_contractAdress).updateXvalue(_x);
         return true;
     }
 
-    function updateValue(
-        ContractA _contractAdress
-    ) public payable returns (bool) {
+    function updateValue(ContractA _contractAdress) public payable returns (bool) {
         //you can also call contract by changing input function contract type
         _contractAdress.updateValue{value: msg.value}();
         return true;
     }
 
-    function updateXandValue(
-        address _contractAdress,
-        string memory _x
-    ) public payable {
+    function updateXandValue(address _contractAdress, string memory _x) public payable {
         ContractA(_contractAdress).updateXandValue{value: msg.value}(_x);
     }
 }
